15899f2a79c748bdb146bcfbbae093c9ab5b1be6
Moch <moch@nodes.dk>
2017-11-18 13:40:54 +0100

    - Refactoring of methods and comments

--- demo/src/main/java/com/muddzdev/styleabletoast/MainActivity.java
+++ demo/src/main/java/com/muddzdev/styleabletoast/MainActivity.java
@@ -1,12 +1,9 @@
 package com.muddzdev.styleabletoast;
 
 import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
 
 import com.muddzdev.styleabletoastlibrary.StyleableToast;
 
